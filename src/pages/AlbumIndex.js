ec40e058ceace9aa85f6cc480e4f290cc8f83888
annihilation.wizard <annihilation.wizard@gmail.com>
2023-05-05 14:58:43 +0800

    update

--- src/pages/AlbumIndex.js
+++ src/pages/AlbumIndex.js
@@ -1,5 +1,16 @@
+import { useOutletContext } from "react-router-dom"
+
+
 export default function AlbumIndex() {
+    const list = useOutletContext();
+    console.log("AlbumIndex", list);
     return (
-        <div>這是相簿首頁</div>
+        <div>這是相簿首頁
+            {list.map((item) => {
+                return (
+                    <li key={item.id}>{item.id}</li>
+                )
+            })}
+        </div>
     )
 }
--- src/pages/AlbumLayout.js
+++ src/pages/AlbumLayout.js
@@ -1,15 +1,37 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 import { Outlet } from "react-router-dom"
 
 
+const api = 'https://api.unsplash.com/search/photos';
+const accessId = process.env.REACT_APP_UNSPLASH_ACCESS;
+console.log(api, accessId);
 
 export default function AlbumLayout() {
+
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await axios.get(`${api}?client_id=${accessId}&query=animal`);
+            const { results } = response.data;
+            console.log(results);
+            setList(results);
+        })();
+    }, [])
+
     return (
         <div className="row">
             <div className="col-4">
                 左側選單列表
+                {list.map((item) => {
+                    return (
+                        <li key={item.id}>{item.id}</li>
+                    )
+                })}
             </div>
             <div className="col-8">
-                <Outlet />
+                <Outlet context={list} />
             </div>
         </div>
     )
